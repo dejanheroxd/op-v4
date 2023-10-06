f7c8df1c8ecf107862b4d5d6fb63b845dd1fdc02
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-06 21:42:04 +0300

    changed button padding bottom of legendaty btn

--- src/components/cards/FilterRarityBar.jsx
+++ src/components/cards/FilterRarityBar.jsx
@@ -2,7 +2,7 @@ function FilterRarityBar() {
   return (
     <div className="flex justify-center w-full mt-8">
       <div className="border  rounded-md border-black w-[335px] text-center flex ">
-        <button className="border-r w-24 rounded-l-md hover:bg-yellow-300 text-sm pb-[2px] border-black px-1">
+        <button className="border-r w-24 rounded-l-md hover:bg-yellow-300 text-sm border-black px-1">
           Common
         </button>
         <button className="border-r w-24 hover:bg-yellow-300 text-sm border-black px-1">
