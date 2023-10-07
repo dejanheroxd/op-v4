0c4e9f8abacd27bf108bf4668637e122d3070afb
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-07 18:29:07 +0300

    added active filter bg and reset x btn

--- src/components/cards/FilterRarityBar.jsx
+++ src/components/cards/FilterRarityBar.jsx
@@ -1,42 +1,61 @@
 import { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
+import { XCircle } from "phosphor-react";
 
 function FilterRarityBar() {
-  const { getClickedRarity } = useContext(ShopContext);
+  const { getClickedRarity, clickedFilterRarity } = useContext(ShopContext);
 
   return (
     <div className="flex justify-center w-full mt-8">
-      <div className="border  rounded-md border-black w-[335px] text-center flex ">
+      <div className="border relative rounded-md border-black w-[335px] text-center flex ">
         <button
           onClick={() => getClickedRarity("Common")}
-          className="border-r w-24 rounded-l-md hover:bg-yellow-300 text-sm border-black px-1"
+          className={`${
+            clickedFilterRarity === "Common" ? "bg-yellow-300" : ""
+          } border-r w-24 rounded-l-md hover:bg-yellow-300 text-sm border-black px-1`}
         >
           Common
         </button>
         <button
           onClick={() => getClickedRarity("Uncommon")}
-          className="border-r w-24 hover:bg-yellow-300 text-sm border-black px-1"
+          className={`${
+            clickedFilterRarity === "Uncommon" ? "bg-yellow-300" : ""
+          } border-r w-24 hover:bg-yellow-300 text-sm border-black px-1`}
         >
           Uncommon
         </button>
         <button
           onClick={() => getClickedRarity("Rare")}
-          className="border-r w-24 hover:bg-yellow-300 text-sm border-black px-1"
+          className={`${
+            clickedFilterRarity === "Rare" ? "bg-yellow-300" : ""
+          } border-r w-24 hover:bg-yellow-300 text-sm border-black px-1`}
         >
           Rare
         </button>
         <button
           onClick={() => getClickedRarity("Epic")}
-          className="border-r w-24 hover:bg-yellow-300 text-sm border-black px-1"
+          className={`${
+            clickedFilterRarity === "Epic" ? "bg-yellow-300" : ""
+          } border-r w-24 hover:bg-yellow-300 text-sm border-black px-1`}
         >
           Epic
         </button>
         <button
           onClick={() => getClickedRarity("Legendary")}
-          className="border-black rounded-r-md hover:bg-yellow-300 text-sm px-1"
+          className={`${
+            clickedFilterRarity === "Legendary" ? "bg-yellow-300" : ""
+          } border-black rounded-r-md hover:bg-yellow-300 text-sm px-1`}
         >
           Legendary
         </button>
+        {clickedFilterRarity !== "" && (
+          <div onClick={() => getClickedRarity("")} className="group">
+            <XCircle
+              size={25}
+              className="absolute group-hover:text-red-500 group-active:text-red-700 duration-100 hover:cursor-pointer top-[-27px] right-[-10px]"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
